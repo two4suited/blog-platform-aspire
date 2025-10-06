4249e5e6d77cc81d0100cbc106878fb278681110
Brian Sheridan <brian.sheridan@gmail.com>
2025-10-06 00:39:49 +0000

    feat: integrate Azure Cosmos DB for weather data storage

--- src/WeatherApp.AppHost/AppHost.cs
+++ src/WeatherApp.AppHost/AppHost.cs
@@ -1,6 +1,20 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var api = builder.AddProject<Projects.WeatherApp_Api>("weatherapi");
+#pragma warning disable ASPIRECOSMOSDB001
+
+var cosmos = builder.AddAzureCosmosDB("cosmos").RunAsPreviewEmulator(
+    emulator =>
+    {
+        emulator.WithDataExplorer();            
+    });
+
+#pragma warning restore ASPIRECOSMOSDB001
+
+var database = cosmos.AddCosmosDatabase("WeatherDB");
+var container = database.AddContainer("WeatherData", "/id");
+
+var api = builder.AddProject<Projects.WeatherApp_Api>("weatherapi")
+    .WithReference(container);
 
 var frontend = builder.AddViteApp("frontend", "../WeatherApp.Web")
     .WithNpmPackageInstallation()
